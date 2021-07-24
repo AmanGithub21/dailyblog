bfbb97d724abe3d6a30bef9b99885077a5126c3f
Aman Porwal <01amanporwal@gmail.com>
2021-07-24 13:28:56 +0530

    Added start script

--- app.js
+++ app.js
@@ -86,6 +86,7 @@ app.use(function(err, req, res, next){
   res.status(statusCode).send(message);
 });
 
-app.listen(3000, function(){
-  console.log("Server running on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, function(){
+  console.log(`Server running on port ${port}`);
 });
